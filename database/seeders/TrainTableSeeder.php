7c9987507228ce11ac3904d5e796f70dcd69b16b
pierobramanti <piero.bramanti94@gmail.com>
2024-09-26 18:37:38 +0200

    [UPD] Add seeder for trains table

--- database/seeders/TrainTableSeeder.php
+++ database/seeders/TrainTableSeeder.php
@@ -2,18 +2,40 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use App\Models\Train;
 
 class TrainTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
     public function run()
     {
-        //
+        
+        $faker = Faker::create();
+
+        for ($i = 0; $i < 30; $i++) {
+            $train = new Train();
+
+           
+            $train->azienda = $faker->company;
+            $train->stazione_di_partenza = $faker->city;
+            $train->stazione_di_arrivo = $faker->city;
+
+            $orarioPartenza = $faker->time();
+            $orarioArrivo = $faker->time();
+
+            while ($orarioArrivo === $orarioPartenza) {
+                $orarioArrivo = $faker->time();
+            }
+
+            $train->orario_di_partenza = $orarioPartenza;
+            $train->orario_di_arrivo = $orarioArrivo;
+            $train->codice_treno = $faker->bothify('??####');
+            $train->numero_carrozze = $faker->numberBetween(4, 12);
+            $train->in_orario = $faker->boolean();
+            $train->cancellato = $faker->boolean();
+
+            $train->save();
+        }
     }
 }
